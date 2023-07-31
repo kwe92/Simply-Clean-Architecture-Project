5559f64ad0562b5b4d676bcac3bb9e0433197c54
Kwe <Kweayon@gmail.com>
2023-07-31 17:59:47 -0500

    update: added @RoutePage annotation to screens

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:working_with_auto_route_guards/shared/base_screen/base_screen.dart';
 
+@RoutePage()
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
   @override
