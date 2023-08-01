19ffb7e4025c32b2e9a83c46c341e27945503907
Kwe <Kweayon@gmail.com>
2023-07-31 21:00:15 -0500

    added view model to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:working_with_auto_route_guards/screens/models/login_screen_view_model.dart';
 import 'package:working_with_auto_route_guards/shared/base_screen/base_screen.dart';
 
 @RoutePage()
@@ -7,8 +9,18 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const BaseScreen(
-        title: 'Login Screen',
-        child: Column(),
-      );
+  Widget build(BuildContext context) => BaseScreen(
+      title: 'Login Screen',
+      child: Consumer(
+        builder: (BuildContext context, LoginScreenViewModel model, _) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                model.someText,
+              )
+            ],
+          );
+        },
+      ));
 }
