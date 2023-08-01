f770e3274f348c950c9359b8e04d155b917d95d2
Kwe <Kweayon@gmail.com>
2023-07-31 23:06:11 -0500

    update: added TextControllers and started imlementing log in function

--- lib/screens/models/login_screen_view_model.dart
+++ lib/screens/models/login_screen_view_model.dart
@@ -1,9 +1,24 @@
 // ignore_for_file: prefer_final_fields
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// TODO: research shared prefs
 
 class LoginScreenViewModel extends ChangeNotifier {
-  String _someText = 'from LoginScreenViewModel';
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
-  String get someText => _someText;
+  TextEditingController get usernameController => _usernameController;
+  TextEditingController get passwordController => _passwordController;
+
+  Future<void> login() async {
+    String? username = usernameController.text.trim().toLowerCase();
+    String? password = passwordController.text.trim();
+
+    if (username == 'gaara' && password == 'sandKing') {
+      SharedPreferences pref = await SharedPreferences.getInstance();
+      pref.setBool('logged_in', true);
+    }
+  }
 }
