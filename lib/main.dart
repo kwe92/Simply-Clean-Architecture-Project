b2a298fbc6db1c8668d4de0465543c2778dfd2e7
Kwe <Kweayon@gmail.com>
2023-07-31 21:01:54 -0500

    implemented change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:working_with_auto_route_guards/routes/app_router.dart';
+import 'package:working_with_auto_route_guards/screens/models/login_screen_view_model.dart';
 
 void main() {
   runApp(_RouteGuardsApp());
@@ -11,13 +13,16 @@ class _RouteGuardsApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: const ColorScheme.light(
-            brightness: Brightness.light,
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (BuildContext context) => LoginScreenViewModel(),
+        child: MaterialApp.router(
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: const ColorScheme.light(
+              brightness: Brightness.light,
+            ),
           ),
+          routerConfig: _appRouter.config(),
         ),
-        routerConfig: _appRouter.config(),
       );
 }
