f42107ef9576becc817565cd1f7235ec926cff81
Kwe <Kweayon@gmail.com>
2023-08-01 11:33:03 -0500

    update: started implementation for onNavigation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,23 +4,26 @@ import 'package:provider/provider.dart';
 import 'package:working_with_auto_route_guards/screens/models/login_screen_view_model.dart';
 import 'package:working_with_auto_route_guards/shared/base_screen/base_screen.dart';
 
+typedef BoolCallback = bool Function(bool?);
+
 @RoutePage()
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final BoolCallback onResult;
+
+  const LoginScreen({required this.onResult, super.key});
 
   @override
   Widget build(BuildContext context) => BaseScreen(
-      title: 'Login Screen',
-      child: Consumer(
-        builder: (BuildContext context, LoginScreenViewModel model, _) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                model.someText,
-              )
-            ],
-          );
-        },
-      ));
+        title: 'Login Screen',
+        child: Consumer(
+          builder: (BuildContext context, LoginScreenViewModel model, _) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(''),
+              ],
+            );
+          },
+        ),
+      );
 }
