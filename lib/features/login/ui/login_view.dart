3c525a7273c90fda9fad2eb82ba2d00d95fee4ac
Kwe <Kweayon@gmail.com>
2023-08-01 21:41:23 -0500

    update: implementing clean architecture | features | login

---
+++ lib/features/login/ui/login_view.dart
@@ -0,0 +1,68 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:working_with_auto_route_guards/features/login/ui/login_view_model.dart';
+import 'package:working_with_auto_route_guards/features/shared/base_scaffold_view.dart';
+import 'package:working_with_auto_route_guards/app/type_declarations/type_declarations.dart';
+
+@RoutePage()
+class LoginView extends StatelessWidget {
+  final BoolVoidCallback onResult;
+
+  const LoginView({required this.onResult, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const double horizontalPadding = 36, verticalPadding = 12;
+
+    return BaseScaffoldView(
+      title: 'Login Screen',
+      child: Consumer(
+        builder: (BuildContext context, LoginScreenViewModel model, _) {
+          return ColoredBox(
+            color: const Color.fromRGBO(141, 142, 154, 0.25),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: model.usernameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Username',
+                    ),
+                  ),
+                  const SizedBox(height: 42),
+                  TextFormField(
+                    controller: model.passwordController,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  OutlinedButton(
+                    onPressed: () {
+                      model.login(
+                        context,
+                        onResult,
+                      );
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        horizontalPadding,
+                        verticalPadding,
+                        horizontalPadding,
+                        verticalPadding,
+                      ),
+                      child: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
