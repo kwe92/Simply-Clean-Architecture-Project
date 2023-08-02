450eab8cd56f3ae5a71f4464f3ebcd68004b0112
Kwe <Kweayon@gmail.com>
2023-08-02 09:18:46 -0500

    update: initial textStyles

---
+++ lib/app/themes/text_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:working_with_auto_route_guards/app/themes/app_theme.dart';
+import 'package:working_with_auto_route_guards/app/themes/colors.dart';
+
+const textStyleHeader0 = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.w600,
+  fontSize: 24,
+  // color: Colors.orange,
+);
+
+const buttonText = TextStyle(
+  fontFamily: fontFamily,
+  fontWeight: FontWeight.w600,
+  fontSize: 18,
+  color: AppColors.purple0,
+);
