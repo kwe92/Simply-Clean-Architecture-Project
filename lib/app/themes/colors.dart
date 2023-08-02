e104286e65db4893dbe63a977eafb8b0fd2e8c37
Kwe <Kweayon@gmail.com>
2023-08-02 09:18:16 -0500

    update: inital app colors

---
+++ lib/app/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // private named constructor; local to the class.
+  AppColors._();
+
+  static const purple0 = Color(0xFF562354);
+  static const grey0 = Color.fromRGBO(141, 142, 154, 0.25);
+}
