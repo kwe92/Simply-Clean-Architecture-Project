0c3836c28780640c91563156658bf81bd2a20713
Kwe <Kweayon@gmail.com>
2023-08-02 22:29:32 -0500

    update: added reusable resources

---
+++ lib/app/resources/reusables.dart
@@ -0,0 +1,33 @@
+// Gaps
+
+import 'package:flutter/material.dart';
+
+// Gaps
+
+const SizedBox gaph4 = SizedBox(height: 4);
+
+const SizedBox gaph8 = SizedBox(height: 8);
+
+const SizedBox gaph12 = SizedBox(height: 12);
+
+const SizedBox gaph16 = SizedBox(height: 16);
+
+const SizedBox gaph24 = SizedBox(height: 24);
+
+const SizedBox gaph32 = SizedBox(height: 32);
+
+const SizedBox gaph42 = SizedBox(height: 42);
+
+const SizedBox gapw4 = SizedBox(width: 4);
+
+const SizedBox gapw8 = SizedBox(width: 8);
+
+const SizedBox gapw12 = SizedBox(width: 12);
+
+const SizedBox gapw16 = SizedBox(width: 16);
+
+const SizedBox gapw24 = SizedBox(width: 24);
+
+const SizedBox gapw32 = SizedBox(width: 32);
+
+const SizedBox gapw42 = SizedBox(width: 42);
