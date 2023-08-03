0d180ae7fef392c9b1da6b9080b26a2bb2f2fc1b
Kwe <Kweayon@gmail.com>
2023-08-03 18:50:41 -0500

    explanation: why consolidating your typedefs are a good idea

--- lib/app/type_declarations/type_declarations.dart
+++ lib/app/type_declarations/type_declarations.dart
@@ -3,3 +3,11 @@ import 'package:auto_route/auto_route.dart';
 typedef BoolVoidCallback = void Function(bool?);
 
 typedef Routes = ({AutoRoute initialRoute, List<AutoRoute> protectedRuotes, Iterable<AutoRoute> otherRoutes});
+
+
+// Type Aliases | interfaces | the shapes you desire
+
+//   - consolidate your type aliases in one module
+//   - you may need to use the same type alias throughout your program
+//   - makes your code look cleaner as you do not have type aliases
+//     defined randomly throughout your program
