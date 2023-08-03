3c517f7e209a19aed07d14308343f54774225884
Kwe <Kweayon@gmail.com>
2023-08-03 18:44:19 -0500

    explanation: text style module

--- lib/app/themes/text_styles.dart
+++ lib/app/themes/text_styles.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:working_with_auto_route_guards/app/themes/app_theme.dart';
 import 'package:working_with_auto_route_guards/app/themes/colors.dart';
 
+// TODO: code use some refactoring
+
 const textStyleHeader0 = TextStyle(
   fontFamily: fontFamily,
   fontWeight: FontWeight.w600,
@@ -13,12 +15,17 @@ final buttonTextStyle = TextStyle(
   fontFamily: fontFamily,
   fontWeight: FontWeight.w600,
   fontSize: 18,
-  // color: AppColors.purple0,
-  // required to use paint with MaterialStateProperty.resolveWith
-  foreground: Paint()..color = AppColors.purple0,
+  foreground: Paint()
+    ..color = AppColors
+        .purple0, //? foreground: Paint()..color = Color is required to change the color of text when using MaterialStateProperty.resolveWith
 );
 
 final TextStyle bodyTextStyle = buttonTextStyle.copyWith(
   fontWeight: FontWeight.w500,
   fontSize: 16,
 );
+
+// TextStyle Module
+
+//   - keep text application text styles in one location
+//   - copy reuseable properties to keep code DRY and save time in the future
