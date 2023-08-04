566da9ca2058c0781d4bed915f8589d7183a06a6
Kwe <Kweayon@gmail.com>
2023-08-04 17:58:02 -0500

    update: changed onSurface color to white | implemented snackBarTheme | added light mode TextForm colors | improved DRYness of code

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:working_with_auto_route_guards/app/themes/colors.dart';
 import 'package:working_with_auto_route_guards/app/themes/text_styles.dart';
@@ -6,43 +7,28 @@ const String fontFamily = 'CircularXX';
 
 // TODO:light mode colors: Navigation back button color | snackbar color | hilighted text color of TextFormField | ensure its not an off white background
 
-// ? completed
-// ?   - on pressed button color (lightBlue1)
-// ?
-// ?
-// ?
-// ?
-// ?
-
 ThemeData? appTheme = getThemeData(isDarkMode: false);
 
 ThemeData getThemeData({required bool isDarkMode}) {
   const double borderWidth = 2.5;
-
+  final appTextStyle = AppTextStyles.getBodyTextStyle(isDarkMode: isDarkMode);
   return ThemeData(
     useMaterial3: true,
     colorScheme: isDarkMode ? darkColorScheme : lightColorScheme,
     appBarTheme: AppBarTheme(
-      titleTextStyle: textStyleHeader0,
+      titleTextStyle: AppTextStyles.textStyleHeader0,
       backgroundColor: isDarkMode ? AppColors.purple0 : AppColors.abyssalBlue,
     ),
     textTheme: TextTheme(
-      bodyLarge: isDarkMode
-          ? bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.purple0)
-          : bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.skyBlue), //? controls TextFormField widget style
-      // bodySmall: TextStyle(color: Colors.orange),
-      bodyMedium: isDarkMode
-          ? bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.purple0)
-          : bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.skyBlue), //? controls Text widget style
-      // titleMedium: TextStyle(color: Colors.orange),
-      // titleLarge: TextStyle(color: Colors.orange),
-      // titleSmall: TextStyle(color: Colors.orange),
+      bodyLarge: appTextStyle, //? controls TextFormField widget style
+      bodyMedium: appTextStyle, //? controls Text widget style
+    ),
+    // change selectionHandleColor on IOS
+    cupertinoOverrideTheme: const CupertinoThemeData(
+      primaryColor: AppColors.skyBlue,
     ),
     inputDecorationTheme: InputDecorationTheme(
-      // border: OutlineInputBorder(), //TODO: changed border type
-      hintStyle: isDarkMode
-          ? bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.purple0)
-          : bodyTextStyle.copyWith(foreground: Paint()..color = AppColors.skyBlue),
+      hintStyle: appTextStyle,
       contentPadding: const EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 0.0),
       enabledBorder: UnderlineInputBorder(
         borderSide: BorderSide(
@@ -59,21 +45,29 @@ ThemeData getThemeData({required bool isDarkMode}) {
     ),
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: isDarkMode ? AppColors.charcol : AppColors.lightBlue0,
+      selectionColor: AppColors.lightBlue1,
     ),
     outlinedButtonTheme: isDarkMode ? purpleOutlinedButton : blueOutlinedButton,
+    snackBarTheme: SnackBarThemeData(
+      shape: ContinuousRectangleBorder(
+        side: BorderSide(
+          color: isDarkMode ? AppColors.purple0 : AppColors.skyBlue,
+          width: borderWidth,
+        ),
+      ),
+    ),
   );
 }
 
 const darkColorScheme = ColorScheme.dark(
   background: AppColors.grey0,
   onBackground: AppColors.purple0, //? affects borders, what else?
-  // surface: Colors.green,
-  onSurface: AppColors.grey0, //? affects Flutter navigation back button what else?
-  // onPrimary: Colors.orange,
+  onSurface: AppColors.grey0, //? affects Flutter navigation back button, snackBar background what else?
 );
 
 const lightColorScheme = ColorScheme.light(
   onBackground: AppColors.skyBlue,
+  onSurface: Colors.white,
 );
 
 OutlinedButtonThemeData purpleOutlinedButton = OutlinedButtonThemeData(
@@ -94,14 +88,9 @@ MaterialStateProperty<T> _resolver<T>(
 
 const double _horizontalPadding = 48, _verticalPadding = 16;
 
-ButtonStyle purpleButtonStyle = ButtonStyle(
+ButtonStyle basicButtonStyle = ButtonStyle(
   shape: _resolver(
     () => const StadiumBorder(),
-  ),
-  // foregroundColor: _resolver(() => null),
-  // backgroundColor: _resolver(() => null),
-  textStyle: _resolver(
-    () => buttonTextStyle.copyWith(foreground: Paint()..color = AppColors.purple0),
   ),
   padding: _resolver(
     () => const EdgeInsets.fromLTRB(
@@ -113,36 +102,24 @@ ButtonStyle purpleButtonStyle = ButtonStyle(
   ),
 );
 
-// TODO: blueButtonStyle could be more DRY
-
-ButtonStyle blueButtonStyle = ButtonStyle(
-  shape: _resolver(
-    () => const StadiumBorder(),
-  ),
-  // foregroundColor: _resolver(() => null),
-  // backgroundColor: _resolver(() => null),
+ButtonStyle purpleButtonStyle = basicButtonStyle.copyWith(
   textStyle: _resolver(
-    () => buttonTextStyle.copyWith(
-      foreground: Paint()..color = AppColors.skyBlue,
+    () => AppTextStyles.buttonTextStyle.copyWith(
+      foreground: Paint()..color = AppColors.purple0,
     ),
   ),
-  padding: _resolver(
-    () => const EdgeInsets.fromLTRB(
-      _horizontalPadding,
-      _verticalPadding,
-      _horizontalPadding,
-      _verticalPadding,
+);
+
+ButtonStyle blueButtonStyle = basicButtonStyle.copyWith(
+  textStyle: _resolver(
+    () => AppTextStyles.buttonTextStyle.copyWith(
+      foreground: Paint()..color = AppColors.skyBlue,
     ),
   ),
   overlayColor: _resolver(
     () => AppColors.lightBlue1,
   ),
 );
-
-final b = blueButtonStyle.copyWith();
-
-
-
 
 
 // --------------------------------------------------------------- //
