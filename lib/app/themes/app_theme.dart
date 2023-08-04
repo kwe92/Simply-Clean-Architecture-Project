b92a8c2a991cdf2a6caf1310f74b87dfc6c03490
Kwe <Kweayon@gmail.com>
2023-08-04 18:14:38 -0500

    update: encapsulated theme in a function

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -5,64 +5,66 @@ import 'package:working_with_auto_route_guards/app/themes/text_styles.dart';
 
 const String fontFamily = 'CircularXX';
 
-// TODO:light mode colors: Navigation back button color | snackbar color | hilighted text color of TextFormField | ensure its not an off white background
+// TODO: Add comments on sections of code that seem to require
 
-ThemeData? appTheme = getThemeData(isDarkMode: false);
+class AppTheme {
+  const AppTheme._();
 
-ThemeData getThemeData({required bool isDarkMode}) {
-  const double borderWidth = 2.5;
-  final appTextStyle = AppTextStyles.getBodyTextStyle(isDarkMode: isDarkMode);
-  return ThemeData(
-    useMaterial3: true,
-    colorScheme: isDarkMode ? darkColorScheme : lightColorScheme,
-    appBarTheme: AppBarTheme(
-      titleTextStyle: AppTextStyles.textStyleHeader0,
-      backgroundColor: isDarkMode ? AppColors.purple0 : AppColors.abyssalBlue,
-    ),
-    textTheme: TextTheme(
-      bodyLarge: appTextStyle, //? controls TextFormField widget style
-      bodyMedium: appTextStyle, //? controls Text widget style
-    ),
-    // change selectionHandleColor on IOS
-    cupertinoOverrideTheme: const CupertinoThemeData(
-      primaryColor: AppColors.skyBlue,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      hintStyle: appTextStyle,
-      contentPadding: const EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 0.0),
-      enabledBorder: UnderlineInputBorder(
-        borderSide: BorderSide(
-          color: isDarkMode ? AppColors.purple0 : AppColors.skyBlue,
-          width: borderWidth,
+  static ThemeData getThemeData({required bool isDarkMode}) {
+    const double borderWidth = 2.5;
+    final appTextStyle = AppTextStyles.getBodyTextStyle(isDarkMode: isDarkMode);
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: isDarkMode ? darkColorScheme : lightColorScheme,
+      appBarTheme: AppBarTheme(
+        titleTextStyle: AppTextStyles.textStyleHeader0,
+        backgroundColor: isDarkMode ? AppColors.purple0 : AppColors.abyssalBlue,
+      ),
+      textTheme: TextTheme(
+        bodyLarge: appTextStyle, // controls TextFormField widget style
+        bodyMedium: appTextStyle, // controls Text widget style
+      ),
+      // change selectionHandleColor on IOS
+      cupertinoOverrideTheme: const CupertinoThemeData(
+        primaryColor: AppColors.skyBlue,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: appTextStyle,
+        contentPadding: const EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 0.0),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: isDarkMode ? AppColors.purple0 : AppColors.skyBlue,
+            width: borderWidth,
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: isDarkMode ? AppColors.charcol : AppColors.lightBlue0,
+            width: borderWidth,
+          ),
         ),
       ),
-      focusedBorder: UnderlineInputBorder(
-        borderSide: BorderSide(
-          color: isDarkMode ? AppColors.charcol : AppColors.lightBlue0,
-          width: borderWidth,
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: isDarkMode ? AppColors.charcol : AppColors.lightBlue0,
+        selectionColor: AppColors.lightBlue1,
+      ),
+      outlinedButtonTheme: isDarkMode ? purpleOutlinedButton : blueOutlinedButton,
+      snackBarTheme: SnackBarThemeData(
+        shape: ContinuousRectangleBorder(
+          side: BorderSide(
+            color: isDarkMode ? AppColors.purple0 : AppColors.skyBlue,
+            width: borderWidth,
+          ),
         ),
       ),
-    ),
-    textSelectionTheme: TextSelectionThemeData(
-      cursorColor: isDarkMode ? AppColors.charcol : AppColors.lightBlue0,
-      selectionColor: AppColors.lightBlue1,
-    ),
-    outlinedButtonTheme: isDarkMode ? purpleOutlinedButton : blueOutlinedButton,
-    snackBarTheme: SnackBarThemeData(
-      shape: ContinuousRectangleBorder(
-        side: BorderSide(
-          color: isDarkMode ? AppColors.purple0 : AppColors.skyBlue,
-          width: borderWidth,
-        ),
-      ),
-    ),
-  );
+    );
+  }
 }
 
 const darkColorScheme = ColorScheme.dark(
   background: AppColors.grey0,
-  onBackground: AppColors.purple0, //? affects borders, what else?
-  onSurface: AppColors.grey0, //? affects Flutter navigation back button, snackBar background what else?
+  onBackground: AppColors.purple0, // affects borders, what else?
+  onSurface: AppColors.grey0, // affects Flutter navigation back button, snackBar background what else?
 );
 
 const lightColorScheme = ColorScheme.light(
