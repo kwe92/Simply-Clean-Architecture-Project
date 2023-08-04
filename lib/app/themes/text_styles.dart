a753516fd5f2b0b5b272b5e4a31ce19a4d660b85
Kwe <Kweayon@gmail.com>
2023-08-04 14:25:02 -0500

    update: DRY clean code

--- lib/app/themes/text_styles.dart
+++ lib/app/themes/text_styles.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:working_with_auto_route_guards/app/themes/app_theme.dart';
-import 'package:working_with_auto_route_guards/app/themes/colors.dart';
 
-// TODO: code use some refactoring
-
-const textStyleHeader0 = TextStyle(
+const baseTextStyle = TextStyle(
   fontFamily: fontFamily,
   fontWeight: FontWeight.w600,
+);
+
+final textStyleHeader0 = baseTextStyle.copyWith(
   fontSize: 24,
-  // color: Colors.orange,
 );
 
-final buttonTextStyle = TextStyle(
-  fontFamily: fontFamily,
-  fontWeight: FontWeight.w600,
+final buttonTextStyle = baseTextStyle.copyWith(
   fontSize: 18,
-  foreground: Paint()
-    ..color = AppColors
-        .purple0, //? foreground: Paint()..color = Color is required to change the color of text when using MaterialStateProperty.resolveWith
 );
 
-final TextStyle bodyTextStyle = buttonTextStyle.copyWith(
+final TextStyle bodyTextStyle = baseTextStyle.copyWith(
   fontWeight: FontWeight.w500,
   fontSize: 16,
 );
