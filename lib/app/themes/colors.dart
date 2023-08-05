6c8eedc258a62098aba2e8f7f2e8641b5c6a4bdb
Kwe <Kweayon@gmail.com>
2023-08-04 21:21:57 -0500

    update: changed all colors to their hexadecimal representation | explained why hexadecimal representations are more readable

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -6,17 +6,19 @@ class AppColors {
 
   static const purple0 = Color(0xFF562354);
 
+  static const purple1 = Color(0x3E562354);
+
   static const grey0 = Color(0xFFE3E8EC);
 
   static const charcol = Color(0xFF36454F);
 
   static const abyssalBlue = Color(0xFF1B2834);
 
-  static const skyBlue = Color.fromRGBO(2, 117, 194, 1);
+  static const skyBlue = Color(0xFF0275C2);
 
-  static const lightBlue0 = Color.fromRGBO(121, 172, 219, 1);
+  static const lightBlue0 = Color(0xFF79ACDB);
 
-  static const lightBlue1 = Color.fromRGBO(121, 172, 219, 0.25);
+  static const lightBlue1 = Color(0x3D79ACDB);
 }
 
 // Colors Class
@@ -25,3 +27,18 @@ class AppColors {
 //   - using const ClassName._() as the named (identity) constructor
 //     prevents the class from being instantiated (created as an object)
 //     without the need to modify the class to be abstract
+
+// TODO: explain base-16 vs base-10
+
+// Hexadecimal Color Representation
+
+//   - app colors above are represented as hexadecimal colors
+//   - the hexadecimal representation of RGB will always have 2 characters
+//     per channel providing consistancy in code readability  
+//   - remove the first 4 characters and replace then with # to find the color online
+
+//   - Conversion Examples:
+
+//       - 0xFF79ACDB -> #79ACDB
+//       - 0xFF1B2834 -> #1B2834
+
