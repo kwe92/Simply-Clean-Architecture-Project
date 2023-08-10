02c2ae8b66c79b16730584a3c9bb0abd8b9af93c
Kwe <Kweayon@gmail.com>
2023-08-10 16:41:51 -0500

    update

--- lib/app/type_declarations/type_declarations.dart
+++ lib/app/type_declarations/type_declarations.dart
@@ -2,7 +2,11 @@ import 'package:auto_route/auto_route.dart';
 
 typedef BoolVoidCallback = void Function(bool?);
 
-typedef Routes = ({AutoRoute initialRoute, List<AutoRoute> protectedRuotes, Iterable<AutoRoute> otherRoutes});
+typedef Routes = ({
+  AutoRoute initialRoute,
+  List<AutoRoute> protectedRuotes,
+  Iterable<AutoRoute> otherRoutes,
+});
 
 
 // Type Aliases | interfaces | the shapes you desire
